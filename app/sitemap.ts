87f3c647f3d21106ea0d706b1995accbdd837d81
Wayjourney <email4open@gmail.com>
2024-07-01 23:38:38 +0800

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,19 +5,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://zzreass.com',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'weekly',
       priority: 1,
     },
     {
       url: 'https://zzreass.com/about',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.5,
+    },
+    {
+      url: 'https://zzreass.com/products',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
     },
     {
       url: 'https://zzreass.com/contact',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
   ]
