03afa0f664121c8b68017f2fda3151c894b79f08
Wayjourney <email4open@gmail.com>
2024-07-01 23:32:55 +0800

    Update products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,7 +4,7 @@ import ProductList from './data';
 export default function Products() {
   return (
     <div className='mx-auto my-20 max-w-screen-xl px-6 lg:px-8'>
-      <h1 className='my-8 text-2xl font-bold text-slate-800'>产品列表</h1>
+      <h1 className='mb-6 text-3xl font-bold text-slate-800'>产品列表</h1>
       <div className='grid grid-cols-2 border-l border-t bg-white sm:mx-0 md:grid-cols-3 lg:grid-cols-4'>
         {ProductList.map((item, index) => (
           <div key={index} className='relative border-b border-r p-4 sm:p-6'>
