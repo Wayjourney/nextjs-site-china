a2f63079f0323207dd39292d2e8c157339bfb98a
Wayjourney <email4open@gmail.com>
2024-07-01 14:08:15 +0800

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://zzreass.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://zzreass.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://zzreass.com/inquiry',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
