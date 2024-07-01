d09ff6ef4012ea478b4aada4025a458d27626bc0
Wayjourney <email4open@gmail.com>
2024-07-01 10:26:01 +0800

    Update resend api key name

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { EmailTemplate } from '@/components/EmailTemplate';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_REASS_API_KEY);
 
 export async function POST(req: Request) {
   const { name, email, message } = await req.json();
