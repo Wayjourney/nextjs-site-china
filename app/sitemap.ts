561114e1e578de932de3c0e789be5c9dfc6bdce8
Wayjourney <email4open@gmail.com>
2024-07-08 00:38:07 +0800

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,23 @@
-import { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+import ProductList from '@/lib/data';
+
+interface SitemapItem {
+  url: string;
+  lastModified?: string | Date;
+  changeFrequency?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+  priority?: number;
+}
+
+const productLinks: SitemapItem[] = ProductList.map((item) => {
+  return {
+    url: `https://zzreass.com/products/${item.id}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }
+})
+
+export default function sitemap(): SitemapItem[] {
+  const baseLinks: SitemapItem[] = [
     {
       url: 'https://zzreass.com',
       lastModified: new Date(),
@@ -27,4 +43,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
   ]
+  
+  return baseLinks.concat(productLinks);
 }
