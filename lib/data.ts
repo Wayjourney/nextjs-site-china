0d4de6e73f581312130f4b95c3240e65428cde31
Wayjourney <email4open@gmail.com>
2024-07-08 00:12:11 +0800

    Update products data

--- lib/data.ts
+++ lib/data.ts
@@ -1,14 +1,14 @@
 const ProductList = [
-  { id: 1, name: '漂珠转砖', description: [] },
-  { id: 2, name: '高强高铝保温砖', description: [] },
-  { id: 3, name: '蛭石保温砖', description: [] },
-  { id: 4, name: '陶瓷蛭石隔热板', description: [] },
-  { id: 5, name: '微孔高强低导热砖', description: [] },
-  { id: 6, name: '莫来石保温砖', description: [] },
-  { id: 7, name: '高铝聚轻砖', description: [] },
-  { id: 8, name: '035超轻绝热保温砖', description: [] },
-  { id: 9, name: '绝缘发泡转', description: [] },
-  { id: 10, name: '防渗砖', description: [] },
+  { id: 1, name: '漂珠转砖', href: '/products/1', description: [] },
+  { id: 2, name: '高强高铝保温砖', href: '/products/2', description: [] },
+  { id: 3, name: '蛭石保温砖', href: '/products/3', description: [] },
+  { id: 4, name: '陶瓷蛭石隔热板', href: '/products/4', description: [] },
+  { id: 5, name: '微孔高强低导热砖', href: '/products/5', description: [] },
+  { id: 6, name: '莫来石保温砖', href: '/products/6', description: [] },
+  { id: 7, name: '高铝聚轻砖', href: '/products/7', description: [] },
+  { id: 8, name: '035超轻绝热保温砖', href: '/products/8', description: [] },
+  { id: 9, name: '绝缘发泡转', href: '/products/9', description: [] },
+  { id: 10, name: '防渗砖', href: '/products/10', description: [] },
 ];
 
 export default ProductList;
