d6e45a91fdee426d0e8e25ac9eff523a34cc6739
Wayjourney <email4open@gmail.com>
2024-07-09 00:13:16 +0800

    Add scroll false to breadcrumb link

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -60,6 +60,7 @@ export default function ProductPage({
                     href={page.href!}
                     aria-current={page.current ? 'page' : undefined}
                     className='ml-4 text-sm font-medium text-gray-500 hover:text-gray-700'
+                    scroll={false}
                   >
                     {page.name}
                   </Link>
