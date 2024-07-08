2b694cd2d4181074d34680ae5429af7ae07b1436
Wayjourney <email4open@gmail.com>
2024-07-08 21:42:06 +0800

    Update viewport meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover" />
         <meta name="baidu-site-verification" content="codeva-zVYXn1UOnJ" />
       </head>
       <body className={inter.className}>
